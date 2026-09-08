b3d54afc4d71cf1fb560d3638865a0b71595a2e2
ahmed_mar <marnissiahmed1911@gmail.com>
2026-09-08 02:09:38 +0100

    update main

--- src/main.py
+++ src/main.py
@@ -6,7 +6,7 @@ load_dotenv(".env")
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 from routes import base , data
-
+from stores.llm.LLMProviderFactory import LLMProviderFactory
 
 app = FastAPI()
 
@@ -15,6 +15,18 @@ async def startup_db_client():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+
+    llm_provider_factory = LLMProviderFactory(settings)
+
+    # generation client
+    app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation_client.set_generation_model(model_id = settings.GENERATION_MODEL_ID)
+
+    # embedding client
+    app.embedding_client = llm_provider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
+                                             embedding_size=settings.EMBEDDING_MODEL_SIZE)
+
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
