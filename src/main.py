fad6e6675aa42c868896b054366cb4c15bde5e5e
ahmed_mar <marnissiahmed1911@gmail.com>
2026-09-11 21:24:45 +0100

    add template parser

--- src/main.py
+++ src/main.py
@@ -5,18 +5,22 @@ load_dotenv(".env")
 
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
-from routes import base , data
+from stores.llm.templates.template_parser import TemplateParser
+from routes import base , data , nlp
 from stores.llm.LLMProviderFactory import LLMProviderFactory
+from stores.vectordb.VectorDBProviderFactory import VectorDBProviderFactory
 
 app = FastAPI()
 
 @app.on_event("startup")
-async def startup_db_client():
+async def startup_span():
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
 
     llm_provider_factory = LLMProviderFactory(settings)
+    vectordb_provider_factory = VectorDBProviderFactory(settings)
+
 
     # generation client
     app.generation_client = llm_provider_factory.create(provider=settings.GENERATION_BACKEND)
@@ -27,10 +31,24 @@ async def startup_db_client():
     app.embedding_client.set_embedding_model(model_id=settings.EMBEDDING_MODEL_ID,
                                              embedding_size=settings.EMBEDDING_MODEL_SIZE)
 
+    # vector db client
+    app.vectordb_client = vectordb_provider_factory.create(
+        provider=settings.VECTOR_DB_BACKEND
+    )
+    app.vectordb_client.connect()
+
+    # template parser
+    app.template_parser = TemplateParser(
+        language=settings.PRIMARY_LANG,
+        default_language=settings.DEFAULT_LANG
+    )
+
 
 @app.on_event("shutdown")
-async def shutdown_db_client():
+async def shutdown_span():
     app.mongo_conn.close()
+    app.vectordb_client.disconnect()
 
 app.include_router(base.base_router)
 app.include_router(data.data_router)
+app.include_router(nlp.nlp_router)
